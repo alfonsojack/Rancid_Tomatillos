fbbe684265aa4e46dc78b8d3ec27c55ced775bf5
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-10-14 17:30:37 -0600

    Update class names on selected movie card

--- src/components/SelectedMoviesCard/SelectedMoviesCard.js
+++ src/components/SelectedMoviesCard/SelectedMoviesCard.js
@@ -8,18 +8,18 @@ const SelectedMoviesCard = ( {selectedMovie}) => {
       <section>
         <img  className='sel-movie-backdrop-img' src={selectedMovie.backdrop_path}></img>
       </section>
-      <p>{selectedMovie.tagline}</p>
+      <p className='sel-movie-tagline'>{selectedMovie.tagline}</p>
       <section className='sel-movie-detail-section'>
         <img className='sel-movie-poster-img' src={selectedMovie.poster_path}></img>
         <div className='sel-movie-title-section'>
           <h2 className='sel-movie-title'>{selectedMovie.title}</h2>
           <p className='sel-movie-overview'>{selectedMovie.overview}</p>
         </div>
-        <div>
+        <div className='sel-movie-details'>
           <p>Rating: {selectedMovie.average_rating} / 10</p>
           <p>Release: {selectedMovie.release_date.split('-', 1)}</p>
           <p>Genres: {selectedMovie.genres.join(' / ')}</p>
-          <p>Runtime:{selectedMovie.runtime} minutes</p>
+          <p>Runtime: {selectedMovie.runtime} minutes</p>
         </div>
       </section>
     </div>
