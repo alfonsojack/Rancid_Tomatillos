738466e6ecc139a271f7436cd736edeb3f44668e
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-10-14 17:07:19 -0600

    Add new movie details to card

--- src/components/SelectedMoviesCard/SelectedMoviesCard.js
+++ src/components/SelectedMoviesCard/SelectedMoviesCard.js
@@ -2,18 +2,24 @@ import './SelectedMoviesCard.css'
 
 
 const SelectedMoviesCard = ( {selectedMovie}) => {
-
+// genres[arr], overview, runtime, tagline
   return (
     <div className='sel-movie-card'>
       <section>
         <img  className='sel-movie-backdrop-img' src={selectedMovie.backdrop_path}></img>
       </section>
+      <p>{selectedMovie.tagline}</p>
       <section className='sel-movie-detail-section'>
         <img className='sel-movie-poster-img' src={selectedMovie.poster_path}></img>
-        <h2>{selectedMovie.title}</h2>
+        <div className='sel-movie-title-section'>
+          <h2 className='sel-movie-title'>{selectedMovie.title}</h2>
+          <p className='sel-movie-overview'>{selectedMovie.overview}</p>
+        </div>
         <div>
-          <p>Rating: {selectedMovie.average_rating}</p>
-          <p>Release: {selectedMovie.release_date}</p>
+          <p>Rating: {selectedMovie.average_rating} / 10</p>
+          <p>Release: {selectedMovie.release_date.split('-', 1)}</p>
+          <p>Genres: {selectedMovie.genres.join(' / ')}</p>
+          <p>Runtime:{selectedMovie.runtime} minutes</p>
         </div>
       </section>
     </div>
