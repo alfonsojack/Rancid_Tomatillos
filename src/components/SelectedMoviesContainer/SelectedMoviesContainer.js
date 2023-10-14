f72f825c4857b04185aadc236dea955fecfcbd6d
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-10-14 11:15:41 -0600

    Refactor props to render single movie from API

--- src/components/SelectedMoviesContainer/SelectedMoviesContainer.js
+++ src/components/SelectedMoviesContainer/SelectedMoviesContainer.js
@@ -4,11 +4,7 @@ import './SelectedMoviesContainer.css'
 const SelectedMoviesContainer = (props) => {
   //map over movieData.movies
   //pluck out values to use as props
-  console.log(props.movies)
-  const selectedMoviesCard = props.movies.find((movie) => {
-    return movie.id === props.selectedMovie
-
-  })
+  const selectedMoviesCard = props.selectedMovie
   console.log('selectedMoviesCard: ', selectedMoviesCard)
   return (
     <div className="selected -movie-grid">      
