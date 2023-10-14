a62560b674dceb8a9c9d67c01e47b9f913d6a81b
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-14 12:26:36 -0600

    Add error handling to our network requests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,14 +14,25 @@ const App = () => {
 
   const getAllMovies = () => {
     fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+      return response.json()
+    })
     .then(data => setMovies(data.movies))
     .catch(error => console.log(error.message))
   }
 
   const getSingleMovie = (movieId) => {
     fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieId}`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+      return response.json()
+    })
     .then(data => setSelectedMovie(data.movie))
     .catch(error => console.log(error.message))
   }
