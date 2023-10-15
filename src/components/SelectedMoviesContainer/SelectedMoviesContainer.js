3b5a930620581eb4543d3f5c273d9a46485bde75
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-10-15 12:45:10 -0600

    Run prop through SelectedMoviesContainer

--- src/components/SelectedMoviesContainer/SelectedMoviesContainer.js
+++ src/components/SelectedMoviesContainer/SelectedMoviesContainer.js
@@ -1,14 +1,13 @@
 import SelectedMoviesCard from '../SelectedMoviesCard/SelectedMoviesCard'
 import './SelectedMoviesContainer.css'
 
-const SelectedMoviesContainer = (props) => {
+const SelectedMoviesContainer = ({ selectedMovie, trailer}) => {
   //map over movieData.movies
   //pluck out values to use as props
-  const selectedMoviesCard = props.selectedMovie
-  console.log('selectedMoviesCard: ', selectedMoviesCard)
+  // const selectedMoviesCard = selectedMovie
   return (
     <div className="selected -movie-grid">      
-      <SelectedMoviesCard selectedMovie={selectedMoviesCard} />
+      <SelectedMoviesCard selectedMovie={selectedMovie} trailer={trailer} />
     </div>
   )
 }
