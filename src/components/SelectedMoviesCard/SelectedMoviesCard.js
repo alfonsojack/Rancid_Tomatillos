05e50cb8e2a0dff45772cc66c9bac0b12de27bab
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-15 10:30:37 -0600

    Add more styling to selected movie page

--- src/components/SelectedMoviesCard/SelectedMoviesCard.js
+++ src/components/SelectedMoviesCard/SelectedMoviesCard.js
@@ -1,19 +1,22 @@
 import './SelectedMoviesCard.css'
 
 
-const SelectedMoviesCard = ( {selectedMovie}) => {
-// genres[arr], overview, runtime, tagline
+const SelectedMoviesCard = ({ selectedMovie }) => {
+  const imgStyle = {
+    backgroundImage: `url(${selectedMovie.backdrop_path})`,
+  };
+
   return (
     <div className='sel-movie-card'>
-      <section>
-        <img  className='sel-movie-backdrop-img' src={selectedMovie.backdrop_path}></img>
-      </section>
-      <p className='sel-movie-tagline'>{selectedMovie.tagline}</p>
+      <section className='img-section' style={imgStyle}>
+        <div className='img-overlay'>
+          {/* <p className='sel-movie-tagline'>{selectedMovie.tagline}</p> */}
       <section className='sel-movie-detail-section'>
         <img className='sel-movie-poster-img' src={selectedMovie.poster_path}></img>
         <div className='sel-movie-title-section'>
           <h2 className='sel-movie-title'>{selectedMovie.title}</h2>
           <p className='sel-movie-overview'>{selectedMovie.overview}</p>
+          <p className='sel-movie-tagline'>{selectedMovie.tagline}</p>
         </div>
         <div className='sel-movie-details'>
           <p>Rating: {selectedMovie.average_rating} / 10</p>
@@ -22,10 +25,11 @@ const SelectedMoviesCard = ( {selectedMovie}) => {
           <p>Runtime: {selectedMovie.runtime} minutes</p>
         </div>
       </section>
+        </div>
+      </section>
+      {/* <p className='sel-movie-tagline'>{selectedMovie.tagline}</p> */}
     </div>
   )
 }
 
 export default SelectedMoviesCard
-
-
