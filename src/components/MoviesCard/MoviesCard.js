7f7c6e4818bea2aef64497ccf70cb4dffcf0bb71
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-16 17:40:53 -0600

    Style the movies page

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -4,9 +4,13 @@ import './MoviesCard.css'
 const MoviesCard = (props) => {
   return (
     <div className="movies-card" onClick={props.onClick}>
-      <h4>{props.title}</h4>
+      <div className='card-bars'>
+        <h4>{props.title}</h4>
+      </div>
       <img className="movies-poster-img" src={props.poster}></img>
-      <p>{props.rating.toFixed(1)}</p>
+      <div className='card-bars'>
+        <p>Rating: {props.rating.toFixed(1)}</p>
+      </div>
     </div>
   )
 }
