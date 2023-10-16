1bc702313137e15c401c4773a233b1498f857fe8
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-16 17:44:16 -0600

    Make minor style changes to selected movie page

--- src/components/SelectedMoviesCard/SelectedMoviesCard.js
+++ src/components/SelectedMoviesCard/SelectedMoviesCard.js
@@ -5,14 +5,16 @@ const SelectedMoviesCard = ({ selectedMovie, trailer }) => {
   const imgStyle = {
     backgroundImage: `url(${selectedMovie.backdrop_path})`,
   };
-  const trailerUrl = `https://www.youtube.com/embed/${trailer.key}?si=KM2o77Gkm7VXdn6F`
+  const trailerUrl = `https://www.youtube.com/embed/${trailer.key}`
   return (
     <div className='sel-movie-card'>
       <section className='img-section' style={imgStyle}>
         <div className='img-overlay'>
           {/* <p className='sel-movie-tagline'>{selectedMovie.tagline}</p> */}
       <section className='sel-movie-detail-section'>
-        <img className='sel-movie-poster-img' src={selectedMovie.poster_path}></img>
+        <div className='sel-movie-poster-section'>
+          <img className='sel-movie-poster-img' src={selectedMovie.poster_path}></img>
+        </div>
         <div className='sel-movie-title-section'>
           <h2 className='sel-movie-title'>{selectedMovie.title}</h2>
           <p className='sel-movie-overview'>{selectedMovie.overview}</p>
