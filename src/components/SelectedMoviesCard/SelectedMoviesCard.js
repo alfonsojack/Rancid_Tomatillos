02e5e8f632b1483ac924839c70d4ddfd39396d9d
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-17 17:46:47 -0600

    Add proptypes for SelectedMoviesCard

--- src/components/SelectedMoviesCard/SelectedMoviesCard.js
+++ src/components/SelectedMoviesCard/SelectedMoviesCard.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import './SelectedMoviesCard.css'
 
 
@@ -41,3 +42,27 @@ const SelectedMoviesCard = ({ selectedMovie, trailer }) => {
 }
 
 export default SelectedMoviesCard
+
+SelectedMoviesCard.propTypes = {
+  selectedMovie: PropTypes.shape({
+    average_rating: PropTypes.number.isRequired,
+    backdrop_path: PropTypes.string.isRequired,
+    budget: PropTypes.number.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+    id: PropTypes.number.isRequired,
+    overview: PropTypes.string.isRequired,
+    poster_path: PropTypes.string.isRequired,
+    release_date: PropTypes.string.isRequired,
+    revenue: PropTypes.number.isRequired,
+    runtime: PropTypes.number.isRequired,
+    tagline: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }),
+  trailer: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    key: PropTypes.string.isRequired,
+    movie_id: PropTypes.number.isRequired,
+    site: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  })
+}
