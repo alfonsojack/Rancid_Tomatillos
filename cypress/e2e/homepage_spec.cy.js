54d6baa0618378f73113c8563b751cbc8fd278d4
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-18 17:53:39 -0600

    Add cypress test for title and movie card number

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('user should be able to visit the homepage', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 200,
+      fixture: "mockMovieData"
+    })
+    cy.visit('http://localhost:3000')
+  })
+  it('homepage should have a title and 3 movies', () => {
+    cy.get('h1').contains('Rancid Tomatillos')
+    cy.get('.movie-grid').find('.movies-card').should('have.length', 4)
+    
   })
 })
