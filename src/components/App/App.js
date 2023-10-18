0df011aca2ffe3d16dc0fcbe618a0934ff909b95
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-10-18 13:43:13 -0600

    Move props and states

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,14 @@ import MoviesContainer from '../MoviesContainer/MoviesContainer'
 import Header from '../Header/Header'
 import SelectedMoviesContainer from '../SelectedMoviesContainer/SelectedMoviesContainer'
 import './App.css'
+import { Routes, Route, Link, useParams } from 'react-router-dom'
 
 
 const App = () => {
   const [movies, setMovies] = useState([])
-  const [selectedMovie, setSelectedMovie] = useState('')
   const [error, setError] = useState('')
-  const [trailer, setTrailer] = useState(null)
-  
 
+  
   const getAllMovies = () => {
     fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
     .then(response => {
@@ -26,60 +25,18 @@ const App = () => {
     .catch(error => setError(error.message))
   }
 
-  const getSingleMovie = (movieId) => {
-    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieId}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`)
-      }
-      return response.json()
-    })
-    .then(data => setSelectedMovie(data.movie))
-    .catch(error => setError(error.message))
-  }
-
-  const getMovieTrailer = (movieId) => {
-    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieId}/videos`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`)
-      }
-      return response.json()
-    })
-    .then(data => {
-      const foundTrailer = data.videos.find((video)=> {return video.type === 'Trailer'});
-      if (foundTrailer) {
-      setTrailer(foundTrailer)
-      } else {
-        setTrailer('NO TRAILER FOUND')
-      }
-    }
-  )
-    .catch(error => setError(error.message))
-  }
-
   useEffect(() => {
     getAllMovies()
   }, [])
 
-  const handleMovieCardClick = (movieId) => {
-    getSingleMovie(movieId)
-    showMovieTrailer(movieId)
-  
-  }
-
-  const showMovieTrailer = (movieId) => {
-    getMovieTrailer(movieId)
-  }
   return ( 
     <div className='App'>
       <Header/>
       {error && <p> {error} </p>}
-      {/* <h2>Hello from App</h2> */}
-      {selectedMovie && trailer ? 
-      <SelectedMoviesContainer selectedMovie={selectedMovie} trailer={trailer}/> 
-      : 
-      <MoviesContainer movies={movies} onMovieCardClick={handleMovieCardClick}/>}
+      <Routes>
+        <Route path="/" element={<MoviesContainer movies={movies}/>}/>
+        <Route path="/movies/:id" element={<SelectedMoviesContainer/>}/>  
+      </Routes>
     </div> 
   )
 }
