42634e72109a33ac02c4628052ed5ac6bcce1825
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-18 19:33:04 -0600

    Add intercepts for all of the network requests for selectedMovie cypress tests

--- cypress/e2e/selectedmoviepage_spec.cy.js
+++ cypress/e2e/selectedmoviepage_spec.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('selectedMoviePage', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+  it('should take us to the selected movies page with that movie\s details', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 200,
+      fixture: "mockMovieData"
+    }).as('movies')
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/1013860', {
+      statusCode: 200,
+      fixture: "mockSelectedMovieData"
+    }).as('singleMovie')
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/1013860/videos', {
+      statusCode: 200,
+      fixture: "mockTrailerData"
+    }).as('trailer')
+    cy.get('.movies-card').eq(1).click().wait('@movies').wait('@singleMovie').wait('@trailer')
   })
 })
