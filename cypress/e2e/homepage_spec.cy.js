00cbc2e1a84fd18205a45b8781cd889dc44406f4
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-18 18:07:06 -0600

    Add cypress test assertions for first movie card

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -9,6 +9,8 @@ describe('user should be able to visit the homepage', () => {
   it('homepage should have a title and 3 movies', () => {
     cy.get('h1').contains('Rancid Tomatillos')
     cy.get('.movie-grid').find('.movies-card').should('have.length', 4)
-    
+    cy.get('.card-bars').first().should('contain', 'Money Plane')
+    cy.get('.card-bars').eq(1).should('contain', 'Rating: 6.7')
+    cy.get('.movies-poster-img').should('have.attr', 'src', "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg")
   })
 })
