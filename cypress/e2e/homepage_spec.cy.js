a5e1ac699b9abbaa4d62bad9fc06ddce710af966
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-18 18:41:36 -0600

    Add sad path for a 404 error

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,12 +1,12 @@
 describe('user should be able to visit the homepage', () => {
   beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+  it('homepage should have a title and 3 movies', () => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
       statusCode: 200,
       fixture: "mockMovieData"
     })
-    cy.visit('http://localhost:3000')
-  })
-  it('homepage should have a title and 3 movies', () => {
     cy.get('h1').contains('Rancid Tomatillos')
     cy.get('.movie-grid').find('.movies-card').should('have.length', 4)
     cy.get('.card-bars').first().should('contain', 'Money Plane')
@@ -16,4 +16,14 @@ describe('user should be able to visit the homepage', () => {
     cy.get('.card-bars').eq(7).should('contain', 'Rating: 7.4')
     cy.get('.movies-poster-img').eq(3).should('have.attr', 'src', "https://image.tmdb.org/t/p/original//4BgSWFMW2MJ0dT5metLzsRWO7IJ.jpg")
   })
+
+  it('should give an error message for a 404 status code', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 404,
+      body: ''
+    })
+    cy.get('p').contains('404 Not Found')
+  })
+
+
 })
