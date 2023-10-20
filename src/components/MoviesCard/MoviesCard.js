35b1947b2ca3a1508fa4f2ea2430978054e3658d
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-10-20 15:05:03 -0600

    Add alt text to movies card

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -3,12 +3,14 @@ import './MoviesCard.css'
 
 
 const MoviesCard = ({ title, rating, poster}) => {
+  const cardAltText = `${title} movie image`
+
   return (
     <div className="movies-card">
       <div className='card-bars'>
         <h4>{title}</h4>
       </div>
-      <img className="movies-poster-img" src={poster}></img>
+      <img className="movies-poster-img" alt={cardAltText} src={poster}></img>
       <div className='card-bars'>
         <p>Rating: {rating.toFixed(1)}</p>
       </div>
