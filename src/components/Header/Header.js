67958897ae2489c25bd8ea545fdd2d2cf9599a78
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-22 12:17:15 -0600

    Add a home button that shows up on pages other than the home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,22 @@
 import './Header.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Header = () => {
-  
+  const location = useLocation()
+
+  console.log('location: ', location)
   return (
     <div className='header-cont'>
       <Link to="/">
       <h1>Rancid Tomatillos</h1>
       </Link>
+      {location.pathname !== "/" &&
+      <Link to="/">
+        <h3>Home</h3>
+      </Link>
+      }
+        
+
     </div>
   )
 }
